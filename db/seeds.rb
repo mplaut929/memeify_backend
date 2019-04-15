7667688b387374e63729faaa68bde20d71c03682
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-04-15 10:10:49 -0400

    completed basic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Meme.find_or_create_by(url: "https://i.imgflip.com/1h7in3.jpg", title: "Smart")
+Meme.find_or_create_by(url: "https://i.imgflip.com/2wifvo.jpg", title: "Unsettling")
+Meme.find_or_create_by(url: "https://i.imgflip.com/1bij.jpg", title: "One does not simply")
+Meme.find_or_create_by(url: "https://i.imgflip.com/26am.jpg", title: "Aliens")
+Meme.find_or_create_by(url: "https://i.imgflip.com/1e7ql7.jpg", title: "Evil Kermit")
+
+30.times do
+  Caption.find_or_create_by(meme_id: rand(2...6), text: Faker::Hipster.sentence, likes: 0)
+end
